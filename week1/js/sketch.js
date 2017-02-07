8660136ff7f9c641d7501d2294ae537734f5ae59
IsaiahGOW <Isaiahderekserrano@gmail.com>
2017-02-07 16:51:55 -0500

    Add files via upload

---
+++ week1/js/sketch.js
@@ -0,0 +1,44 @@
+function setup() {
+    createCanvas(640,480);
+    
+    background('white'); 
+  
+    rect(200,20,380,200);
+    
+    stroke('red');
+    strokeWeight(20);
+    line(200,50,580,50);
+    
+        stroke('red');
+    strokeWeight(20);
+    line(200,100,580,100);
+    
+        stroke('red');
+    strokeWeight(20);
+    line(200,150,580,150);
+    
+        stroke('red');
+    strokeWeight(20);
+    line(200,200,580,200);
+    
+    stroke('blue');
+    fill('blue');
+    triangle( 200,65,400,120,200,195);
+    
+    stroke('white');
+    strokeWeight(4);
+   line(250, 100, 290, 150);
+    
+      stroke('white');
+    strokeWeight(4);
+   line(280, 100, 260, 150);
+    
+    stroke('white');
+    strokeWeight(4);
+   line(270, 90, 270, 160);
+    
+     
+    stroke('white');
+    strokeWeight(4);
+   line(230, 140, 310, 115);
+}
